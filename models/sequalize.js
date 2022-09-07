758d73ea0e6b1d0f54b76b84f2b29d4f4efaf57d
KonstantinBobrovski <KonstantinBobrovski@gmail.com>
2022-09-07 12:34:27 +0300

    changed to remote

--- models/sequalize.js
+++ models/sequalize.js
@@ -1,11 +1,14 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(
-    'postgresql://postgres:admin@localhost/postgres', {
+    'postgres://wtkfoesjfkfasv:b5c8d042a8d7c3f5120f11021dda73bfdc8bfbf7ae97cadbf4ca0c580023da6d@ec2-34-251-115-141.eu-west-1.compute.amazonaws.com:5432/dt1opl8e0folh', {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-        ssl:false
+        ssl:{
+            require: true,
+            rejectUnauthorized: false
+        }
     },
     pool: {
         max: 19,
